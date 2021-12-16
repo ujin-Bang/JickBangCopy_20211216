9c5d81f67dfffb5df4c774ee72218af4e309ca44
ujin-Bang <qkddnwls14@gmail.com>
2021-12-16 15:00:12 +0900

    어댑터 변수 객체화 + 리스트뷰의 어댑터로 연결

--- app/src/main/java/com/neppplus/jickbangcopy_20211216/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211216/MainActivity.kt
@@ -2,11 +2,14 @@ package com.neppplus.jickbangcopy_20211216
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20211216.adapters.RoomAdapter
 import com.neppplus.jickbangcopy_20211216.datas.RoomData
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mRooms = ArrayList<RoomData>()
+    lateinit var mRoomAdapter : RoomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         mRooms.add( RoomData(4300,"서울시 중구",-2,"네번째 방입니다"))
         mRooms.add( RoomData(175300,"서울시 송파구",20,"다섯번째 방입니다"))
 
+        mRoomAdapter = RoomAdapter(this,R.layout.room_list_item, mRooms)
+        roomListView.adapter = mRoomAdapter
 
 
     }
