81a922c674545251cc74ba6af22d3604d90c8ecd
ujin-Bang <qkddnwls14@gmail.com>
2021-12-16 14:56:39 +0900

    방 목록 생성 -> 방 데이터 5개 추가

--- app/src/main/java/com/neppplus/jickbangcopy_20211216/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20211216/MainActivity.kt
@@ -2,10 +2,23 @@ package com.neppplus.jickbangcopy_20211216
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20211216.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRooms = ArrayList<RoomData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRooms.add( RoomData(8000,"서울시 동대문구", 5,"1번째 방입니다."))
+        mRooms.add( RoomData(18000,"서울시 서대문구",0,"두번째 방입니다."))
+        mRooms.add( RoomData(29700,"경기도 고양시",17,"세번째 방입니다."))
+        mRooms.add( RoomData(4300,"서울시 중구",-2,"네번째 방입니다"))
+        mRooms.add( RoomData(175300,"서울시 송파구",20,"다섯번째 방입니다"))
+
+
+
     }
 }
